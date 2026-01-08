7d383ab4e8f201972c58eb7ff8fb1f639f7d9bf3
Quirin Brändli <qbraendli@pm.me>
2026-01-08 03:24:29 +0100

    Make option import of disko

--- hosts/nixos-hosts.nix
+++ hosts/nixos-hosts.nix
@@ -93,8 +93,10 @@ let
         # Other modules
         inputs.impermanence.nixosModules.impermanence
         inputs.disko.nixosModules.disko
+
         # Import host-specific disko configuration if it exists
-        (lib.optional (builtins.pathExists (./${host}/disko.nix)) (./${host}/disko.nix))
+        ++ lib.optional (builtins.pathExists ./${host}/disko.nix) ./${host}/disko.nix
+
         inputs.sops-nix.nixosModules.sops
         inputs.qnix-pkgs.nixosModules.default
 
