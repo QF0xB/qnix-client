6743c59bbd54b6cb2ce3d0f90c5ff2fb696b545c
Quirin Brändli <qbraendli@pm.me>
2026-09-16 19:14:46 +0200

    fix(qpcv1): update qpcv1

--- hosts/QPCv1/qnix.nix
+++ hosts/QPCv1/qnix.nix
@@ -3,13 +3,15 @@
   pkgs,
   inputs,
   ...
-}:
-{
+}: {
   qnix = {
     desktop = {
       hyprland = {
         noHardwareCursors = true;
       };
+    };
+    dev = {
+      jetbrains.rider.enable = true;
     };
     system = {
       boot-manager = {
@@ -31,7 +33,7 @@
           group = user;
           home = "/home/${user}";
           description = "Quirin Brändli";
-          extraGroups = [ "wheel" ];
+          extraGroups = ["wheel"];
           passwordFromSops = "up";
         };
       };
@@ -68,7 +70,7 @@
             mode = "0400";
             owner = "root";
             group = "root";
-            restartUnits = [ "nix-daemon.service" ];
+            restartUnits = ["nix-daemon.service"];
           };
           github_token = {
             key = "github_token";
@@ -91,7 +93,7 @@
     };
 
     network = {
-      networkmanager.extraPlugins = [ "networkmanager-openvpn" ];
+      networkmanager.extraPlugins = ["networkmanager-openvpn"];
 
       wireguard = {
         enable = true;
@@ -99,9 +101,8 @@
 
         tunnels.qf0xb = {
           interfaceName = "wg0";
-          autoconnect = true;
-          addresses = [ "10.100.10.2/32" ];
-          dns = [ "10.10.10.254" ];
+          addresses = ["10.100.10.2/32"];
+          dns = ["10.10.10.254"];
           privateKey.sopsSecret = "qpcv1-wg-qf0xb-private";
           listenPort = 51820;
           mtu = 1320;
@@ -110,7 +111,7 @@
             publicKey = "qE8kYQ6pd35CFjaaf8BbKyFdkJIhlX5N0x7WmOqivkU=";
             presharedKey.sopsSecret = "qpcv1-wg-qf0xb-psk";
             endpoint = "vpn.qf0xb.de:51820";
-            allowedIPs = [ "10.10.10.0/24" ];
+            allowedIPs = ["10.10.10.0/24"];
           };
         };
       };
