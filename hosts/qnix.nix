260e14f3c967672ab86064c4d739346b8cc8dd70
Quirin Brändli <qbraendli@pm.me>
2026-09-17 01:13:03 +0200

    chore(client): add nix-community cache

--- hosts/qnix.nix
+++ hosts/qnix.nix
@@ -2,9 +2,13 @@
 {
   # Client-wide defaults. A host can replace any of these in its own qnix.nix.
   nix.settings = {
-    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-substituters = [
+      "https://cache.numtide.com"
+      "https://nix-community.cachix.org"
+    ];
     extra-trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
