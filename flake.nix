fc37b005320daa197d1879f1bdefe8c8ee2022c8
Quirin Brändli <qbraendli@pm.me>
2026-09-20 16:25:27 +0200

    chore(modules): update to 0.15.4

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
 
     qnix-modules = {
       # Managed by qnix-dev-modules and qnix-use-release.
-      url = "https://flakehub.com/f/QF0xB/qnix-modules/0.15.3";
+      url = "https://flakehub.com/f/QF0xB/qnix-modules/0.15.4";
       inputs.qnix-sdk.follows = "qnix-sdk";
     };
   };
